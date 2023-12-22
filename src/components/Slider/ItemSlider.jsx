129f20a606456a6a2f2c1ba4a7cd1e4608f6f145
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2023-12-22 14:40:12 +0200

    update slider effect

---
+++ src/components/Slider/ItemSlider.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './Slider.module.scss';
+
+import { ReactComponent as ArrowLeftUp } from '../../img/slider/arrow-rightUp.svg';
+
+const Item = ({ e, i }) => {
+   return (
+      <div className={s.eachSlideEffect} key={`${i}slider`}>
+         <img src={e.img} alt={`img Item${i}`} />
+         <div className={s.titleContainer}>
+            <h3>{e.title}</h3>
+            <ArrowLeftUp />
+         </div>
+         <div className={s.textContainer}>
+            <p>{e.text}</p>
+            <p>{e.data}</p>
+         </div>
+      </div>
+   );
+};
+
+export default Item;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ItemSlider from './ItemSlider';
 import img from '../../img/slider/beautiful1.jpg';
 import img2 from '../../img/slider/beautiful2.jpg';
 import img3 from '../../img/slider/beautiful3.jpg';
+import img4 from '../../img/slider/Mask group.jpg';
+import img5 from '../../img/slider/Mask group (1).jpg';
 
 import { ReactComponent as ArrowRight } from '../../img/slider/arrow-right.svg';
 import { ReactComponent as ArrowLeft } from '../../img/slider/arrowleft.svg';
-import { ReactComponent as ArrowLeftUp } from '../../img/slider/arrow-rightUp.svg';
 
 import s from './Slider.module.scss';
 
@@ -29,7 +31,37 @@ const Slider = () => {
          data: 'November 2023',
          img: img3,
       },
+      {
+         title: 'Kherson city Private Enterprise “HealthyFarm”',
+         text: 'Wind power',
+         data: 'September 2021',
+         img: img4,
+      },
+      {
+         title: 'Zaporizhia city Private Enterprise “Biotech”',
+         text: 'Mini nuclear stations',
+         data: 'May 2021',
+         img: img5,
+      },
    ];
+
+   const [slideIndex, setSlideIndex] = useState(1);
+
+   const nextSlide = () => {
+      if (slideIndex !== images.length) {
+         setSlideIndex(slideIndex + 1);
+      } else if (slideIndex === images.length) {
+         setSlideIndex(1);
+      }
+   };
+
+   const prevSlide = () => {
+      if (slideIndex !== 1) {
+         setSlideIndex(slideIndex - 1);
+      } else if (slideIndex === 1) {
+         setSlideIndex(images.length);
+      }
+   };
 
    return (
       <div className={s.container}>
@@ -38,28 +70,33 @@ const Slider = () => {
                <span>01</span>/05
             </div>
             <div className={s.buttonContainer}>
-               <button className={s.buttonSvg}>
+               <button
+                  onClick={() => {
+                     prevSlide();
+                  }}
+                  className={s.buttonSvg}
+               >
                   <ArrowRight />
                </button>
-               <button className={s.buttonSvg}>
+               <button
+                  onClick={() => {
+                     nextSlide();
+                  }}
+                  className={s.buttonSvg}
+               >
                   <ArrowLeft />
                </button>
             </div>
          </div>
-         <div className={s.slidereffect} indicators={true}>
+         <div className={s.sliderEffect} indicators={true}>
             {images.map((e, i) => {
                return (
-                  <div className={s.eachSlideEffect} key={`${i}slider`}>
-                     <img src={e.img} alt="" width="300" />
-                     <div className={s.titleContainer}>
-                        <h3>{e.title}</h3>
-                        <ArrowLeftUp />
-                     </div>
-                     <div className={s.textContainer}>
-                        <p>{e.text}</p>
-                        <p>{e.data}</p>
-                     </div>
-                  </div>
+                  <ItemSlider
+                     e={e}
+                     i={i}
+                     key={`ItemSlider ${i}`}
+                     className={slideIndex === i + 1 ? 'slide active-anim' : 'slide'}
+                  />
                );
             })}
          </div>
